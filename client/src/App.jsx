e7b072d9d63f42f6c4a41fd69b34c87107fa477e
Anders Newgard <anne9707@colorado.edu>
2023-12-19 17:37:27 -0500

    imported necessary packages from ApolloClient into app.jsx. exported index.js files from schemas directory.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,13 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache, HttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import Navbar from './components/Navbar';
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000/graphql',
+});
 
 function App() {
   return (
