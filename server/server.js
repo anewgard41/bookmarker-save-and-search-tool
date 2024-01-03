a9e190f509c34658b99bd11ba4294ec439028948
Anders Newgard <anne9707@colorado.edu>
2024-01-03 16:51:19 -0500

    render deployment debugging, edited server.js file

--- server/server.js
+++ server/server.js
@@ -30,9 +30,6 @@ const startApolloServer = async () => {
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
   };
 
 
